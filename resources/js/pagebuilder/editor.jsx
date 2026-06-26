e0fc475d203226fe77748bf6bee63cd8a5d2f581
HashimovKamran <kamranhesimov9@gmail.com>
2026-06-26 16:39:17 +0400

    guard editor mount

--- resources/js/pagebuilder/editor.jsx
+++ resources/js/pagebuilder/editor.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import Workspace from './workspace.jsx';
-
 const mount = document.getElementById('altura-page-builder-root');
-if (!mount) throw new Error('Page Builder mount point is missing.');
-createRoot(mount).render(<Workspace mount={mount} />);
+if (mount) createRoot(mount).render(<Workspace mount={mount} />);
