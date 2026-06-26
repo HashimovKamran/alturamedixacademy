f0bc23f13965522ae6722fa630cf44f8e8eafd83
HashimovKamran <kamranhesimov9@gmail.com>
2026-06-26 17:10:35 +0400

    feat page builder pass real preview URL

--- resources/views/admin/page_builder/react.blade.php
+++ resources/views/admin/page_builder/react.blade.php
@@ -2,7 +2,7 @@
 @section('title', 'Visual Page Builder')
 @section('page_title', 'Visual Page Builder')
 @section('content')
-<div id="altura-page-builder-root" data-api-base="{{ url('/admin/page-editor/api') }}" data-canvas-url="{{ url('/admin/page-editor/canvas') }}?lang_code={{ urlencode($language) }}" data-page-key="{{ $pageKey }}" data-language="{{ $language }}"></div>
+<div id="altura-page-builder-root" data-api-base="{{ url('/admin/page-editor/api') }}" data-canvas-url="{{ url('/admin/page-editor/canvas') }}?lang_code={{ urlencode($language) }}" data-preview-url="{{ $previewUrl }}" data-page-key="{{ $pageKey }}" data-language="{{ $language }}"></div>
 @endsection
 @push('scripts')
 @vite('resources/js/app.js')
