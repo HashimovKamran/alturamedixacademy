2950b8f8bacee6d7db006f02ff3261ef10046417
HashimovKamran <kamranhesimov9@gmail.com>
2026-06-26 18:17:55 +0400

    refactor(page-builder): remove retired canvas view

--- resources/views/admin/page_builder/canvas.blade.php
+++
@@ -1,9 +0,0 @@
-<!doctype html>
-<html lang="{{ $language }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width,initial-scale=1">
-    @vite('resources/js/app.js')
-</head>
-<body><div id="altura-page-builder-canvas"></div></body>
-</html>
