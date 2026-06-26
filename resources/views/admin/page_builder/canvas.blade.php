addf425639308b963e2f247b8a3be228cfec5466
HashimovKamran <kamranhesimov9@gmail.com>
2026-06-26 16:37:53 +0400

    feat page builder add React preview canvas view

---
+++ resources/views/admin/page_builder/canvas.blade.php
@@ -0,0 +1,2 @@
+<!doctype html>
+<html lang="{{ $language }}"><head><meta charset="utf-8"><meta name="viewport" content="width=device-width,initial-scale=1">@vite(['resources/css/page-builder-react.css','resources/js/pagebuilder/canvas.jsx'])</head><body><div id="altura-page-builder-canvas"></div></body></html>
