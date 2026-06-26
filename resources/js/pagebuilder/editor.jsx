db1f05574645a32845d9f51e13aa90362b3ab760
HashimovKamran <kamranhesimov9@gmail.com>
2026-06-26 16:36:33 +0400

    feat page builder mount React workspace

--- resources/js/pagebuilder/editor.jsx
+++ resources/js/pagebuilder/editor.jsx
@@ -1,1 +1,7 @@
 import React from 'react';
+import { createRoot } from 'react-dom/client';
+import Workspace from './workspace.jsx';
+
+const mount = document.getElementById('altura-page-builder-root');
+if (!mount) throw new Error('Page Builder mount point is missing.');
+createRoot(mount).render(<Workspace mount={mount} />);
