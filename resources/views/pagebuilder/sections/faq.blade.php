004e236f4b22c330ff7202879781b43707d4f1ad
HashimovKamran <kamranhesimov9@gmail.com>
2026-06-26 19:10:19 +0400

    render nested FAQ items

--- resources/views/pagebuilder/sections/faq.blade.php
+++ resources/views/pagebuilder/sections/faq.blade.php
@@ -1,3 +1,13 @@
 @pbSchema(['name' => 'faq.blade'])
-@if($content['title']??false)<h2>{{ $content['title'] }}</h2>@endif
-<div class="pb-faq">@foreach($content['items']??[] as $item)<details><summary>{{ $item['title']??'' }}</summary><p>{{ $item['text']??'' }}</p></details>@endforeach</div>
+@if($content['title'] ?? false)<h2>{{ $content['title'] }}</h2>@endif
+<div class="pb-faq">
+@if($children->isNotEmpty())
+@foreach($children as $child)
+<details><summary>{{ $child['block']->title ?? '' }}</summary><p>{{ $child['block']->text ?? '' }}</p></details>
+@endforeach
+@else
+@foreach((array)($content['items'] ?? []) as $item)
+<details><summary>{{ $item['title'] ?? '' }}</summary><p>{{ $item['text'] ?? '' }}</p></details>
+@endforeach
+@endif
+</div>
