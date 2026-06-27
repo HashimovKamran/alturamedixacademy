44c233cbddb66b37c4ddc0e2069c06bf3c81e7e9
HashimovKamran <kamranhesimov9@gmail.com>
2026-06-27 04:55:28 +0400

    style(home): add final matching stylesheet

--- resources/views/public/home.blade.php
+++ resources/views/public/home.blade.php
@@ -6,6 +6,7 @@
 @push('styles')
 <link rel="stylesheet" href="{{ asset('css/alturamedix-home-reference.css') }}">
 <link rel="stylesheet" href="{{ asset('css/alturamedix-home-final.css') }}">
+<link rel="stylesheet" href="{{ asset('css/alturamedix-home-match.css') }}">
 @endpush
 
 @section('content')
